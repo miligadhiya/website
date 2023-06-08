276553164cdabe2c5016d658db9df7eafc2dcaca
miligadhiya <gadhiyajay493@gmail.com>
2023-06-08 16:10:18 +0530

    project

--- componends/nav.js
+++ componends/nav.js
@@ -2,7 +2,7 @@ let nav=()=>{
     return`<a href="/index.html"><p>HOME</p></a>
     <a href="../pages/signup.html"><p>SIGHNUP</p></a>
     <a href="../pages/login.html"><p>LOGIN</p></a>
-    <a href="../pages/card.html"><p>CARD</p></a>
+    <a href="../pages/cart.html"><p>CART</p></a>
     <a href="../pages/pro.html"><p>product</p></a>`
 }
 
--- script/cart.cjs
+++
@@ -1,3 +0,0 @@
-import nav from "../componeds/nav.js";
-
-document.getElementById("nav").innerHTML=nav()
